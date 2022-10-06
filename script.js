5ef6446879706d90d52f632c738c199e49704503
Machiko <106935371+Machikon@users.noreply.github.com>
2022-10-06 02:16:41 -0700

    Added lines of code to the body section.
    script.js was created.  Starter code for html and css was provided.
    added the timeblock, color, and save button.  Still need to work on saving notes in local storage.

---
+++ script.js
@@ -0,0 +1,9 @@
+var today = moment("10/6/2022");
+$("#currentDate").text(today.format("dddd, MMMM Do YYYY"));
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+var toDo = 
+
+localStorage.setItem("text");
